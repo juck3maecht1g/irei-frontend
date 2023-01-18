3b68032ffa4ce2c2108bbf3c68760b100447de96
ucfjb <94857495+ucfjb@users.noreply.github.com>
2023-01-18 12:19:44 +0100

    removed node_modules

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-//import { LobbyPage } from './lobbyPage/LobbyPage';
-import ChooseLabPage from './chooseLabPage/ChooseLabPage';
+import LobbyPage from './lobbyPage/LobbyPage';
 
 export default function App (){
     return (
       <div>
         <BrowserRouter>
         <Routes >
-          <Route index element={<ChooseLabPage />} />
+          <Route index element={<LobbyPage />} />
         </Routes>
       </BrowserRouter>
       </div>
