2e918e1e7f54fc2702f783e3052bfb0f9dcb9754
Simonhil <sihi@outlook.de>
2023-01-18 16:39:06 +0100

    Pinging from LobyPage works

--- src/lobbyPage/LobbyPage.tsx
+++ src/lobbyPage/LobbyPage.tsx
@@ -1,13 +1,49 @@
-import React from "react";
+import React, {Component} from "react";
+import { useNavigate } from 'react-router-dom';
+
+
+
+const intervalltime = 1000
+const fetchAdress = "http://127.0.0.1:5000/"
+const redirection = "/choosefolder"
+const successCode = 200
+//
+function PingBackend() {
+    const navigate = useNavigate();
+      // fetch data
+      const repeatedFunktion = setInterval(
+      async function dataFetch () {
+        const data = await (
+          await fetch(
+            fetchAdress
+          )
+        ).status;
+          console.log(data)
+          if(data == successCode) {
+            clearInterval(repeatedFunktion)
+            navigate(redirection)
+          }
+      }
+      , intervalltime)
+      
+}
+
 
 
 function LobbyPage() {
+   
+
+  
+        return (  
+            <div>
+                LobbyPage
+                {PingBackend()}
+            </div>
+
+        )
     
-    return (  
-        <div>
-            lobbypage
-        </div>
-    );
+
+
     
 }export default LobbyPage;
 
