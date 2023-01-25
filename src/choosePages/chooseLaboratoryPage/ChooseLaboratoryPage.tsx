d5ca6fbb71e9296b48fbd974c7b74e65d303f421
Simonhil <sihi@outlook.de>
2023-01-25 09:53:06 +0100

    fetch labs withb robot list as ips

--- src/choosePages/chooseLaboratoryPage/ChooseLaboratoryPage.tsx
+++ src/choosePages/chooseLaboratoryPage/ChooseLaboratoryPage.tsx
@@ -1,21 +1,50 @@
-import React from 'react'
+import { data } from 'browserslist'
+import React, { useEffect, useState } from 'react'
+const fetchAdress = "http://127.0.0.1:5000/test"
+const Massage = ""
 
 /**
  * The ChooseLaboratoryPage is used to choose 
  * the Laboratory the user wants to work in
  */
-class ChooseLaboratoryPage extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    state = {  }
-    render() { 
-        return ( 
-            <div>
-                
-            </div>
-         );
-    }
+export function ChooseLaboratoryPage() {
+    var [labs, setLabs] = useState(new Map()); // key = labname, value = robot list
+    var [fetched, setfetched] = useState(false)
+    getLabs(setLabs, fetched, setfetched)
+
+    return ( 
+      
+      <div>
+        
+      </div>
+    );
+
+    
+  
+    
 }
- 
 export default ChooseLaboratoryPage;
+
+
+
+
+async function getLabs(setLabs, fetched, setfetched){
+  if(fetched) {
+    return
+  }
+   setfetched (true)
+    const data = await (
+      await fetch(
+        fetchAdress
+      )
+    ).json().then(data => {
+      var labs = new Map()
+      for(let i = 0; i < data.length; i++) {
+        labs.set(data[i].name, data[i].robots)
+      }
+      setLabs(labs)
+    }
+    )
+   
+        // set state when the data received
+}
