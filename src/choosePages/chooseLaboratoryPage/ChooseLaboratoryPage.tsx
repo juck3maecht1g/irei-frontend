643f62dac7a5138240e51b5ddf7e6f3e43c51f62
Simonhil <sihi@outlook.de>
2023-01-26 15:06:05 +0100

    labPage

--- src/choosePages/chooseLaboratoryPage/ChooseLaboratoryPage.tsx
+++ src/choosePages/chooseLaboratoryPage/ChooseLaboratoryPage.tsx
@@ -1,22 +1,27 @@
-import React from 'react'
-import TopBar from '../../TopBar';
 import { data } from 'browserslist'
 import React, { useEffect, useState } from 'react'
+import TopBar from '../../TopBar';
 const fetchAdress = "http://127.0.0.1:5000/test"
 const Massage = ""
-
 /**
  * The ChooseLaboratoryPage is used to choose 
  * the Laboratory the user wants to work in
  */
-class ChooseLaboratoryPage extends React.Component {
-    render() { 
-        return ( 
-            <div>
-                <TopBar title="Choose Laboratory"></TopBar>
-            </div>
-         );
-    }
+export function ChooseLaboratoryPage() {
+    var [labs, setLabs] = useState(new Map()); // key = labname, value = robot list
+    var [fetched, setfetched] = useState(false)
+    getLabs(setLabs, fetched, setfetched)
+
+    return ( 
+      
+      <div>
+        <TopBar title="Choose Laboratory"></TopBar>
+      </div>
+    );
+
+    
+  
+    
 }
 export default ChooseLaboratoryPage;
 
