5057c53259bb7a572e5c71020b502cff58f17ae9
Simonhil <sihi@outlook.de>
2023-01-29 13:54:17 +0100

    setrobots should be complete

--- src/choosePages/chooseRobotPage/SetRobots.tsx
+++ src/choosePages/chooseRobotPage/SetRobots.tsx
@@ -1,16 +1,25 @@
 
 const postAdressExpRobots = "/api/set_exp_robots"
-const postAdressControllGripperRobots= "/api/set_change_gripper_robots"
+const postAdressChangeGripperRobots= "/api/set_change_gripper_robots"
 const postAdressSavePositionRobots = "/api/set_save_position_robot"
 
 
 
+export async function SetExpRobots(robotIps){
+    informRobots(robotIps, postAdressExpRobots)
+  }
+export async function SetChangeGripperRobots(robotIps){
+informRobots(robotIps, postAdressChangeGripperRobots)
+}
+export async function SetSavePositionRobots(robotIps){
+informRobots(robotIps, postAdressSavePositionRobots)
+}
 
-  /** used to send arrays of ip adreses or single ip adresses
-   * 
-   * @param robotIp 
-   * @param postAdress 
-   */
+/** used to send arrays of ip adreses or single ip adresses
+ * 
+ * @param robotIp 
+ * @param postAdress 
+ */
 async function informRobots (robotIp, postAdress){
     const response = await fetch(postAdress, {
         'method': 'POST',
