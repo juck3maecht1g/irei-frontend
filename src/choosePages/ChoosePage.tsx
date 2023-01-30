a1ec86f0423aa3deb31b826661145abf2146a312
Simonhil <sihi@outlook.de>
2023-01-30 13:46:53 +0100

    fixed title in ChoosePage

--- src/choosePages/ChoosePage.tsx
+++ src/choosePages/ChoosePage.tsx
@@ -2,12 +2,10 @@ import React from 'react'
 import TopBar from '../TopBar';
 
 
-class ChoosePage extends React.Component {
-    render() { 
-        return (
-            <TopBar title={this.props.title}></TopBar>
-        );
-    }
+function ChoosePage (props) {
+    return (
+        <TopBar title={this.props.title}></TopBar>
+    );
 }
  
 export default ChoosePage;
