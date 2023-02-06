94dbfd795def508405c3ded1064defcc65bc48b6
ujgoa <ujgoa@student.kit.edu>
2023-02-06 17:33:32 +0100

    chooseDirPage

--- src/choosePages/chooseDirectoryPage/ChooseDirectoryPage.tsx
+++ src/choosePages/chooseDirectoryPage/ChooseDirectoryPage.tsx
@@ -4,8 +4,6 @@ import TopBar from '../../TopBar';
 import DirectoryButton from './DirectoryButton';
 import { GetDirectories, NavigateDown, NavigateUP } from '../Other FetchAndSet';
 import { Link } from 'react-router-dom';
-import { constants } from 'os';
-import { dir } from 'console';
 
 /**
  * The ChooseDirectoryPage is used to navigate in a Datastructure
@@ -14,41 +12,18 @@ import { dir } from 'console';
 export default function ChooseDirectoryPage () {
     const [directories, setDirectories] = useState(new Map<String,[]>());
     GetDirectories(setDirectories);
-    console.log(directories)
     var registerArray = directories.get("to_navigate")
-    console.log(registerArray)
  
 
-    function help () {
-            if (registerArray !== undefined) {
-                return registerArray.map((number) => {
-                    return <DirectoryButton name={number} />
-                })
-            }
-            return []
-        }
-        
    
-    /*const [button, setButtons] = useState (directories.get("to_navigate")
-    .map((number) => {
-        return <DirectoryButton name={number}/>
-    }))
-
-   */
-    const navUp = (folder) => {
-        NavigateUP(setDirectories);
-        const arrNew = directories.get("to_navigate")
-        setButtons(arrNew?.map((number)=> {
-            return <DirectoryButton name={number} action = {navDown}/>
-        }))
+    const navUp = () => {
+ //       NavigateUP(setDirectories);
+        window.location.reload()
     }
 
     const navDown = (folder) => {
-        NavigateDown(setDirectories, folder);
-        const arrNew = directories.get("to_navigate")
-        setButtons(arrNew?.map((number)=> {
-            return <DirectoryButton name={number.value} action = {navDown}/>
-        }))
+ //       NavigateDown( folder);
+        window.location.reload()
     }
     
 
@@ -57,11 +32,13 @@ export default function ChooseDirectoryPage () {
         <div>
             <TopBar title="Choose Directory"></TopBar>
             <div>
-                {registerArray?.map((number) => {
-                    return <DirectoryButton name={number} action = {navDown}/>
-                })}
+                {
+                    directories.get("to_navigate")?.map((number) => {
+                    return <DirectoryButton name={number} action={navDown}/>
+                    })
+                }
             </div>
-            <button>navigate up</button>
+            <button onClick = {navDown}>navigate up</button>
             <Link to = {"/ChooseLaboratoryPage"}>
                 <button>confirm</button>
             </Link>
@@ -69,3 +46,6 @@ export default function ChooseDirectoryPage () {
      );
 }
 
+/**registerArray?.map((number) => {
+                    return <DirectoryButton name={number} action = {navDown}/>
+                }) */
--- src/choosePages/chooseDirectoryPage/DirectoryButton.tsx
+++ src/choosePages/chooseDirectoryPage/DirectoryButton.tsx
@@ -7,6 +7,7 @@ import React from 'react'
 export default function DirectoryButton (props) {
 
     const execute = () => {
+        props.action(props.name)
     }
 
     return ( 
